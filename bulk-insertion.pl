5df7b4af3bdf550989268cc18e2b1711ffde3e7e
Julio Cesar Souza Murta <souzamurtaj@gmail.com>
2024-07-31 17:59:20 -0300

    Start api requests

--- bulk-insertion.pl
+++ bulk-insertion.pl
@@ -1,7 +1,16 @@
 #!/usr/bin/env perl
 
 use strict;
+use warnings;
 use File::Basename;
+use LWP::UserAgent;
+use HTTP::Request;
+use HTTP::Response;
+use JSON;
+
+our $Domain   = "http://localhost:64569/api";
+our $UserName = "admin";
+our $Password = "Admin\@2020";
 
 sub start {
     print "*************************************************************************************************\n";
@@ -12,9 +21,78 @@ sub start {
     print "*************************************************************************************************\n";
 }
 
+sub api_login {
+    my $url = $Domain . "/account/login";
+    my $encoded = to_json({
+        name     => $UserName,
+        password => $Password
+    });
+
+    my $user_agent = LWP::UserAgent->new();
+    my $request = HTTP::Request->new('POST', $url);
+    $request->header('Content-Type'   => 'application/json');
+    $request->content($encoded);
+
+    my $response = $user_agent->request($request);
+    if ($response->is_success) {
+        print "\nSuccessfully Authenticated." . $response->decoded_content;            
+    } else {
+        print STDERR $response->status_line, "\n";
+    }
+
+    return $response->is_success;
+}
+
+sub api_logout {
+    my $url = $Domain . "/account/logout";
+
+    my $user_agent = LWP::UserAgent->new();
+    my $request = HTTP::Request->new('POST', $url);    
+    $request->header('Content-Length'   => '0');
+
+    my $response = $user_agent->request($request);
+
+    if ($response->is_success) {
+        print "\nLogout Successful." . $response->decoded_content;
+    } else {
+        print STDERR $response->status_line, "\n";
+    }
+
+    return $response->is_success;
+}
+
+sub send_customer_request {
+    my $url = $Domain . "/customers";
+    my $encoded = to_json({       
+	    name => "rock lee",
+	    documentNumber => "123123123",
+        email => "teste\@teste.com"
+    });
+
+    my $user_agent = LWP::UserAgent->new();
+    my $request = HTTP::Request->new('POST', $url);    
+    $request->header('Content-Type'   => 'application/json');
+    $request->content($encoded);
+
+    my $response = $user_agent->request($request);
+    if ($response->is_success) {
+        print "\nCustomer Saved Successfully." . $response->decoded_content;
+    } else {        
+        print STDERR $response->status_line, "\n";
+    }
+}
+
+sub send_employee_request {
+
+}
+
+sub send_order_request {
+
+}
+
 sub save_line {
     my @infos = split('|', shift);    
-    my $line_type = @infos[0];
+    my $line_type = $infos[0];
 
     if ($line_type == 1) {
         print "\nIt's a customer";
@@ -60,5 +138,15 @@ sub process_records {
 }
 
 
-start;
-process_records(@ARGV[0]);
+#start;
+#process_records(@ARGV[0]);
+
+#api_login;
+ 
+    
+if(api_login){
+   send_customer_request;
+   # api_logout;
+}
+
+
